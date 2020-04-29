2a62856124f8596a9f5e8ef081f84c3197cbf8d9
NathanRomanos <nathanromanos@hotmail.com>
2020-04-29 16:02:45 +1200

    created varieties page

---
+++ varieties.php
@@ -0,0 +1,61 @@
+<?php
+  $title = "Varieties";
+  $description = "Different varieties of coffee that we sell";
+
+  require("templates/header.php");
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<div class="myContainer">
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\A_small_cup_of_coffee.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">A simple long black coffee.</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\Cafe-Au-Lait.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">This is a beautifully made capachino coffee.</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\caffe_americano.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">This is one of our americano coffees.</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\galao_kaffee_portugal.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">One of our portugeese style coffees.</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\irish coffee.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">One of our world famous Irish coffees.</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="images\Coffee\white-chocolate-peppermint-mocha.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text" style="color: #764d30">Relax with one of our many flavours of mochachinos.</p>
+    </div>
+  </div>
+
+</div>
+
+<?php
+  require("templates/footer.php");
+?>
